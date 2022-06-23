de2ffa143d6b9357984340da822eb6c055a124fb
Alex Di Guida <alexandre.diguida@gmail.com>
2022-06-23 13:26:34 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 import '../styles/app.css'
 import Experience from './Experience'
 
+console.log('🎉', 'Project generated using vite-three-starter')
+console.log(':: https://github.com/Alex-DG/vite-three-starter ::')
+
 /**
  * Documentations
  */
